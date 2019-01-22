ec55ca1536ece403bd372f3f7c6e79bff5cdc526
Joanna Kolańczyk <asiatbg@gmail.com>
2019-01-22 17:14:31 +0100

    Task1

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,14 @@
 /* Here goes your JS code */
+const showPopupForm = document.getElementById('show-popup-form');
+const closeBtn = document.getElementById('closeBtn');
+const popupContainer = document.getElementById('popupContainer');
+
+showPopupForm.addEventListener('click', () => {
+    popupContainer.style.display = "block";
+    showPopupForm.style.display = "none";
+});
+
+closeBtn.addEventListener('click', () => {
+    popupContainer.style.display = "none";
+    showPopupForm.style.display = "block";
+});
